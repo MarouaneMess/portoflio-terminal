ba0655381a086e1601063dc863a135b640a29da8
marouane <20244973@etud.univ-evry.fr>
2024-11-30 12:25:42 +0100

    second commit

--- constants.ts
+++ constants.ts
@@ -3,3 +3,4 @@ export const hostname = import.meta.env.VITE_HOSTNAME ?? "portfolio";
 export const path = import.meta.env.VITE_PATH ?? "~";
 export const symbol = import.meta.env.VITE_SYMBOL ?? "$";
 export const themes: string[] = ["ubuntu", "matrix", "arch"];
+// Path: index.ts
